15f3ac895774d58577ddb199378761f3d1775622
Buket Çakır <buket-cakir@hotmail.com>
2024-04-18 16:18:32 +0300

    Update StudentRepository.java

--- gradebook/src/main/java/com/bukeetcakir/gradebook/repository/StudentRepository.java
+++ gradebook/src/main/java/com/bukeetcakir/gradebook/repository/StudentRepository.java
@@ -2,9 +2,10 @@ package com.bukeetcakir.gradebook.repository;
 
 import com.bukeetcakir.gradebook.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findStudentByNumber(String number);
-
 
 }
